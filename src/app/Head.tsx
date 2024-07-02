8b29adf379858edfaac48659fedf712518a5d8f8
Rineta2 <rzkir571@gmail.com>
2024-07-02 23:37:53 +0700

    rizki

--- src/app/Head.tsx
+++ src/app/Head.tsx
@@ -44,6 +44,20 @@ const Head: React.FC = () => {
       <meta name="googlebot" content="index, follow" />
       <meta name="google-site-verification" content={metadata.verification} />
 
+      <script type="text/javascript">
+        {`
+        var atOptions = {
+          'key' : '27e54543a309a306bcd155d138c3ddc3',
+          'format' : 'iframe',
+          'height' : 250,
+          'width' : 300,
+          'params' : { }
+        };
+        `}
+      </script>
+
+      <script type="text/javascript" src="//www.topcreativeformat.com/27e54543a309a306bcd155d138c3ddc3/invoke.js" async></script>
+
       <Script
         id="gtm-script"
         strategy="beforeInteractive"
